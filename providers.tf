534b85cb1d03823ad674057d28e05e46fd56b0d0
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2023-01-11 15:17:55 +1100

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-southeast-2"
+  default_tags {
+    tags = {
+      uuid    = "kMeGEoxP8Eo2fadWKpr1PY"
+      kotahi-link = "https://url-link/kMeGEoxP8Eo2fadWKpr1PY"
+    }
+  }
+}
